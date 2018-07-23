c5400797e8dfe82b187c1274cf93926c445dc4f8
Wouter van der Beek (wovander) <wovander@cisco.com>
2018-07-23 13:56:14 +0100

    added update repo script

---
+++ advanced.md
@@ -0,0 +1,12 @@
+# Advanced usage
+Scripts that are intended for usage after initial setup.
+
+# update_repos.sh
+The update_repos.sh script updates the: 
+- tooling repos
+- datamodels repos
+
+The script does NOT update the code repos:
+- iotivity-lite
+The code repo iotivity is pulled at start up with a specific version.
+To get a fresh copy, delete the folder and rerun the install_IOTivity.sh script.
---
+++ update_repos.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -x #echo on
+#############################
+#
+#    copyright 2018 Open Connectivity Foundation, Inc. All rights reserved.
+#    Redistribution and use in source and binary forms, with or without modification,
+#    are permitted provided that the following conditions are met:
+#    1.  Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#    2.  Redistributions in binary form must reproduce the above copyright notice,
+#        this list of conditions and the following disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#
+#    THIS SOFTWARE IS PROVIDED BY THE OPEN CONNECTIVITY FOUNDATION, INC. "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
+#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE OPEN INTERCONNECT CONSORTIUM, INC. OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+#    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#############################
+CURPWD=`pwd`
+
+#
+# tools
+#
+cd ../DeviceBuilder
+# clone the repo
+git pull
+cd $CURPWD
+
+cd ../swagger2x
+# clone the repo
+git pull
+cd $CURPWD
+
+#
+# Data models
+#
+
+cd ../core
+# clone the repo
+git pull
+cd $CURPWD
+
+cd ../IoTDataModels
+# clone the repo
+git pull
+cd $CURPWD
+
