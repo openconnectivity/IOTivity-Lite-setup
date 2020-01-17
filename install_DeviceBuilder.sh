40239cbcfc680f158a3a02a506e572de12316055
Wouter <WAvdBeek@users.noreply.github.com>
2020-01-17 11:47:50 +0800

    Update install_DeviceBuilder.sh
    changed default pki from manufactorer to OCF (kyrio) certs

--- install_DeviceBuilder.sh
+++ install_DeviceBuilder.sh
@@ -66,7 +66,7 @@ echo "cp ./device_output/code/server_introspection.dat.h ./iotivity-lite/include
 echo "if [ ! -f ./pki_certs.zip ]; then" >> gen.sh
 echo "# only create when the file does not exist" >> gen.sh
 echo "#sh ./pki.sh " >> gen.sh
-echo "sh ./manufacturer_pki.sh" >> gen.sh
+echo "sh ./pki.sh" >> gen.sh
 echo "echo \" \" ">> gen.sh
 echo "fi"  >> gen.sh
 
