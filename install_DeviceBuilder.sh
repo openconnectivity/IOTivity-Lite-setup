e044ddb6d30076e5a9b828ad1d5e3f2fde2b65a1
Wouter van der Beek (wovander) <wovander@cisco.com>
2020-03-25 11:59:42 +0000

    IDD reading from file, fix for windows

--- install_DeviceBuilder.sh
+++ install_DeviceBuilder.sh
@@ -82,7 +82,7 @@ echo "  cp ./iotivity-lite/include/server_introspection.dat.h ./iotivity-lite/in
 echo "fi" >> gen.sh
 echo "cp ./device_output/code/server_introspection.dat.h ./iotivity-lite/include/server_introspection.dat.h " >> gen.sh
 echo "# copy over the IDD data file in the windows solution folder" >> gen.sh
-echo "cp ./device_output/code/server_introspection.dat ./iotivity-lite/port/windows/vs2015/server_introspection.dat" >> gen.sh
+echo "cp ./device_output/code/server_introspection.dat ./iotivity-lite/port/windows/vs2015/server_introspection.cbor" >> gen.sh
 echo "#" >> gen.sh
 echo "# create the pki include file (if it does not exist)" >> gen.sh
 echo "if [ ! -f ./pki_certs.zip ]; then" >> gen.sh
